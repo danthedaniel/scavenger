a21116e553adb37dbe04664c6d1ed7ef304d665a
Daniel Angell <me@danangell.com>
2024-11-03 17:46:59 -0800

    Remove gray background

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,7 @@ export default function Document() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <body className="min-h-screen bg-gray-100">
+      <body className="min-h-screen">
         <Main />
         <NextScript />
       </body>
