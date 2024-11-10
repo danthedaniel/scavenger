7c2eefa3b648a33518be6031774e88862df0910d
Daniel Angell <me@danangell.com>
2024-11-09 19:33:58 -0800

    Clean up camera component

--- components/camera.tsx
+++ components/camera.tsx
@@ -5,6 +5,92 @@ import jsQR from "jsqr";
 
 import Button from "~/components/button";
 
+/**
+ * Load the media stream into the video element and wait for it to start playing.
+ * @param video - The video element to load the media stream into
+ * @returns The {@link MediaStream}
+ */
+async function loadMediaStream(video: HTMLVideoElement) {
+  const mediaStream = await startCamera();
+  if (!mediaStream) {
+    throw new Error("Could not access camera");
+  }
+
+  video.srcObject = mediaStream;
+  await new Promise((resolve) => video.addEventListener("playing", resolve));
+
+  return mediaStream;
+}
+
+/**
+ * Start the camera
+ * @returns The {@link MediaStream} or `null` if an error occurred
+ */
+async function startCamera() {
+  try {
+    return await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: "environment" },
+      audio: false,
+    });
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+/**
+ * Scan for a QR code in the video stream
+ * @param video - The video element to scan
+ * @param context - The canvas context to draw the video frame to
+ * @returns The QR code data or `null` if no QR code was found
+ */
+async function scan(
+  video: HTMLVideoElement,
+  context: CanvasRenderingContext2D
+) {
+  const canvas = context.canvas;
+  // Match canvas size to video size
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  while (video.isConnected && video.srcObject) {
+    await frame(video);
+
+    if (video.readyState !== video.HAVE_ENOUGH_DATA) {
+      continue;
+    }
+
+    // Draw video frame to canvas
+    const boundingBox = [0, 0, video.videoWidth, video.videoHeight] as const;
+    context.drawImage(video, ...boundingBox);
+    const videoFrame = context.getImageData(...boundingBox);
+
+    // Scan for QR code
+    const code = jsQR(videoFrame.data, videoFrame.width, videoFrame.height, {
+      inversionAttempts: "dontInvert",
+    });
+    if (!code) continue;
+
+    return code.data;
+  }
+
+  return null;
+}
+
+/**
+ * Wait for the next video frame to be available
+ * @param video - The video element to wait for
+ */
+async function frame(video: HTMLVideoElement) {
+  await new Promise((resolve) => {
+    if ("requestVideoFrameCallback" in video) {
+      video.requestVideoFrameCallback(resolve);
+    } else {
+      requestAnimationFrame(resolve);
+    }
+  });
+}
+
 interface CameraProps {
   onScan: (result: string) => void;
   onClose: () => void;
@@ -19,27 +105,30 @@ function Camera({ onClose, onScan }: CameraProps) {
 
   // Start up the camera feed
   useEffect(() => {
-    if (!mediaStream) {
-      loadMediaStream()
-        .then(setMediaStream)
-        .catch((err) => {
-          console.error(err);
-          setError("Could not access camera");
-        });
-      return;
-    }
-
     const video = videoRef.current;
+    if (!video) return;
+
+    loadMediaStream(video)
+      .then(setMediaStream)
+      .catch((err) => {
+        console.error(err);
+        setError("Could not access camera");
+      });
 
     return () => {
-      mediaStream.getTracks().forEach((track) => track.stop());
+      setMediaStream((mediaStream) => {
+        if (mediaStream) {
+          mediaStream.getTracks().forEach((track) => track.stop());
+        }
+
+        return null;
+      });
 
       if (video) {
         video.srcObject = null;
       }
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mediaStream]);
+  }, []);
 
   // Scan for QR codes
   useEffect(() => {
@@ -53,71 +142,14 @@ function Camera({ onClose, onScan }: CameraProps) {
     if (!videoRef.current) return;
     const video = videoRef.current;
 
-    scan(video, context).catch((err) => {
-      console.error(err);
-      setError("Could not scan QR code");
-    });
+    scan(video, context)
+      .then((qrData) => qrData && onScan(qrData))
+      .catch((err) => {
+        console.error(err);
+        setError("Could not scan QR code");
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [mediaStream]);
-
-  async function loadMediaStream() {
-    if (!videoRef.current) return null;
-    const video = videoRef.current;
-
-    const mediaStream = await startCamera();
-    if (!mediaStream) {
-      throw new Error("Could not access camera");
-    }
-
-    video.srcObject = mediaStream;
-    await new Promise((resolve) => video.addEventListener("playing", resolve));
-
-    return mediaStream;
-  }
-
-  async function startCamera() {
-    try {
-      return await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "environment" },
-        audio: false,
-      });
-    } catch (err) {
-      console.error(err);
-      return null;
-    }
-  }
-
-  async function scan(
-    video: HTMLVideoElement,
-    context: CanvasRenderingContext2D
-  ) {
-    const canvas = context.canvas;
-    // Match canvas size to video size
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    while (video.isConnected && video.srcObject) {
-      await new Promise((resolve) => requestAnimationFrame(resolve));
-
-      if (video.readyState !== video.HAVE_ENOUGH_DATA) {
-        continue;
-      }
-
-      // Draw video frame to canvas
-      const boundingBox = [0, 0, video.videoWidth, video.videoHeight] as const;
-      context.drawImage(video, ...boundingBox);
-      const videoFrame = context.getImageData(...boundingBox);
-
-      // Scan for QR code
-      const code = jsQR(videoFrame.data, videoFrame.width, videoFrame.height, {
-        inversionAttempts: "dontInvert",
-      });
-      if (!code) continue;
-
-      onScan(code.data);
-      break;
-    }
-  }
 
   return (
     <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-black">
